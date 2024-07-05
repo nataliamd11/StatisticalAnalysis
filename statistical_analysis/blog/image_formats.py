d37a9e9565032b2c995928e61c2c6d17631c34d7
nataliamd11 <nataliamdenk11@gmail.com>
2024-07-05 20:15:15 -0300

    feat: adding thumbnail format

---
+++ statistical_analysis/blog/image_formats.py
@@ -0,0 +1,4 @@
+# image_formats.py
+from wagtail.images.formats import Format, register_image_format
+
+register_image_format(Format('thumbnail', 'Thumbnail', 'richtext-image thumbnail', 'max-120x120'))
